477c57fcc2d63206f5de07d8d60fe3b8edbec683
daythekid <37392632+daythekid@users.noreply.github.com>
2018-12-19 16:58:10 -0500

    code with support for controller, but still buggy

--- Game.py
+++ Game.py
@@ -3,19 +3,21 @@ import sys
 import cv2
 import numpy as np
 
-pygame.init()
+joystick = pygame.joystick.Joystick(0)
+joystick.init()
 
-display_width = 1920
-display_height = 960
+face_cascade = cv2.CascadeClassifier("face.xml")
+
+camera = cv2.VideoCapture(0)
+
+
+
+
 
 pygame.mixer.pre_init(44100,16,2,4096)
+pygame.init()
 pygame.display.set_caption("Pro Boxer 5")
-win = pygame.display.set_mode((display_width,display_height))
-
-title = pygame.image.load("Pro-Boxer-.png")
-title_rect = title.get_rect()
-begin_prompt = pygame.image.load("begin prompt.png")
-begin_prompt_rect = begin_prompt.get_rect()
+win = pygame.display.set_mode((1920,960))
 
 player_x = 100
 player_y = 660
@@ -25,35 +27,26 @@ height = 300
 bag_x = 1750
 bag_y = 660
 
+
+
+#Music
+pygame.mixer.music.load("song.mp3")
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(-1)
+
+run = True
+
+
+
 isJump = False
 jumpCount = 10
 
-global punchCount, maxarmLength
 isPunch = False
 punchCount = 10
 maxarmLength = (punchCount ** 2)
-'''
-#Music
-pygame.mixer.music.load("Music.mp3")
-pygame.mixer.music.set_volume(0.5)
-pygame.mixer.music.play(-1)
-'''
-isMenu = True
-run = True
-
-def punch():
-    if punchCount >= -10:
-        neg = 1
-        if jumpCount < 0:
-            neg = -1
-        armLength = maxarmLength - (punchCount ** 2) * neg
-        punchCount -= 1
-        pygame.draw.rect(win,(255,0,0),(player_x + width, player_y + 100, armLength, 50))
-    else:
-        isPunch = False
-        punchCount = 10
 
 def jump():
+    global jumpCount,isJump,player_y
     if jumpCount >= -10:
         neg = 1
         if jumpCount < 0:
@@ -64,46 +57,64 @@ def jump():
         isJump = False
         jumpCount = 10
 
+def punch():
+    global punchCount,maxarmLength, isPunch
+    if punchCount >= -10:
+        neg = 1
+        if punchCount < 0:
+            neg = -1
+        armLength = maxarmLength - (punchCount ** 2) * neg
+        punchCount -= 1
+        pygame.draw.rect(win,(255,0,0),(player_x + width, player_y + 100, armLength, 50))
+    else:
+        isPunch = False
+        punchCount = 10
+
+
 while run:
-    
+
     pygame.time.delay(10)
     
     #Break Condtion
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
-    if isMenu:
-        win.blit(title, (display_width/2 - title_rect.width/2, display_height/6))
-        win.blit(begin_prompt, (display_width/2 - begin_prompt_rect.width/2, 6*display_height/7))
-        pygame.display.update()
-        continue
+    
+    global position
+    ret,img = camera.read()
+    #Converts video feed to gray
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray,1.3,5)
+    for (x,y,w,h) in faces:
+        position = (1920- (x+(w/2))*3)
+        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        cv2.imshow('img',img)
+    k = cv2.waitKey(30) & 0xff
     
     #Refresh Background
     win.fill((0))
-    
     #Movement for rectangle
     keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_RIGHT] and player_x + width < bag_x:
-        player_x += 5
-    if keys[pygame.K_LEFT]:
-        player_x -= 5 
-
-    if not isPunch:
-        if keys[pygame.K_UP]:
-            isPunch = True
-    else:
-        punch()
-
-    if not isJump:
-        if keys[pygame.K_SPACE]:
-            isJump = True
-    else:
-        jump()
+    try:
+        player_x = position
+    except:
+        player_x = 0
     
+    for event in pygame.event.get():
+        if not isPunch:
+            if joystick.get_button(0) == 1:
+                isPunch = True
+        else:
+            punch()
+
+        if not isJump:
+            if keys[pygame.K_SPACE]:
+                isJump = True
+        else:
+            jump()
     pygame.draw.rect(win,(0,255,0),(bag_x,bag_y,width,height))
     pygame.draw.rect(win,(255,0,0),(player_x,player_y,width,height))
     pygame.display.update()
-    
-    
+
+camera.release()
+cv2.distoryAllWindows()
