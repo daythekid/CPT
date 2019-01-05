82956221a000f7e7a286caba8ad6d0cb41bc73fc
daythekid <37392632+daythekid@users.noreply.github.com>
2019-01-05 18:29:28 -0500

    Fixed facial recognition

--- opencv.py
+++ opencv.py
@@ -1,20 +1,19 @@
 import cv2
 import numpy as np
 
+face_cascade = cv2.CascadeClassifier("face.xml")
+
 camera = cv2.VideoCapture(0)
 
 while True:
-    ret,frame =camera.read()
-    hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    ret,img = camera.read()
+    #Converts video feed to gray
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray,1.3,5)
 
-    lower_colour = np.array([0,69,100])
-    higher_colour = np.array([80,165,255])
-
-    mask = cv2.inRange(hsv,lower_colour, higher_colour)
-    cv2.imshow("Masked", mask)
-
-    if cv2.waitKey(1) == 27:
-        break
+    for (x,y,w,h) in faces:
+        position = x+(w/2)
+        print(position)
 
 camera.release()
-cv2.distroyallwindows()
+cv2.distoryAllWindows()
