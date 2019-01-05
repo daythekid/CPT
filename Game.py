47fc1473753be5298af0f7f9985f01c67e3c792e
daythekid <37392632+daythekid@users.noreply.github.com>
2019-01-05 18:14:27 -0500

    Joystick control added, still bugs
    Punching and Jumping variables need to be fixed (isJump and isPunch need to be built into the functions)

--- Game.py
+++ Game.py
@@ -1,23 +1,35 @@
 import pygame
 import sys
-import cv2
-import numpy as np
 
-joystick = pygame.joystick.Joystick(0)
-joystick.init()
-
-face_cascade = cv2.CascadeClassifier("face.xml")
-
-camera = cv2.VideoCapture(0)
-
-
-
-
-
+# Music and intial variables
 pygame.mixer.pre_init(44100,16,2,4096)
 pygame.init()
+pygame.joystick.init()
 pygame.display.set_caption("Pro Boxer 5")
-win = pygame.display.set_mode((1920,960))
+
+displayWidth = 1920
+displayHeight = 960
+
+win = pygame.display.set_mode((displayWidth,displayHeight))
+
+pygame.mixer.music.load("song.mp3")
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(-1)
+
+run = True
+
+isJump = False
+jumpCount = 10
+
+isPunch = False
+punchCount = 10
+maxarmLength = (punchCount ** 2)
+
+title = pygame.image.load("Pro-Boxer-.png")
+title_rect = title.get_rect()
+begin_prompt = pygame.image.load("begin prompt.png")
+begin_prompt_rect = begin_prompt.get_rect()
+isMenu = True
 
 player_x = 100
 player_y = 660
@@ -27,23 +39,6 @@ height = 300
 bag_x = 1750
 bag_y = 660
 
-
-
-#Music
-pygame.mixer.music.load("song.mp3")
-pygame.mixer.music.set_volume(0.5)
-pygame.mixer.music.play(-1)
-
-run = True
-
-
-
-isJump = False
-jumpCount = 10
-
-isPunch = False
-punchCount = 10
-maxarmLength = (punchCount ** 2)
 
 def jump():
     global jumpCount,isJump,player_y
@@ -70,29 +65,32 @@ def punch():
         isPunch = False
         punchCount = 10
 
-
 while run:
-
-    pygame.time.delay(10)
-    
-    #Break Condtion
     for event in pygame.event.get():
+        if event.type == pygame.JOYBUTTONDOWN:
+            print("pressed")
+        if event.type == pygame.JOYBUTTONUP:
+            print("released")
         if event.type == pygame.QUIT:
             run = False
     
-    global position
-    ret,img = camera.read()
-    #Converts video feed to gray
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray,1.3,5)
-    for (x,y,w,h) in faces:
-        position = (1920- (x+(w/2))*3)
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        cv2.imshow('img',img)
-    k = cv2.waitKey(30) & 0xff
-    
+    joystick = pygame.joystick.Joystick(0)
+    joystick.init()
+    jump_button = joystick.get_button( 0 )
+    punch_button = joystick.get_button( 1 )
+
+    if isMenu:
+        win.blit(title, (displayWidth/2 - title_rect.width/2, displayHeight/6))
+        win.blit(begin_prompt, (displayWidth/2 - begin_prompt_rect.width/2, 6*displayHeight/7))
+        pygame.display.update()
+        
+        
+        if jump_button == 1 or punch_button == 1:
+            isMenu = False
+
     #Refresh Background
     win.fill((0))
+    
     #Movement for rectangle
     keys = pygame.key.get_pressed()
     try:
@@ -100,21 +98,14 @@ while run:
     except:
         player_x = 0
     
-    for event in pygame.event.get():
-        if not isPunch:
-            if joystick.get_button(0) == 1:
-                isPunch = True
-        else:
-            punch()
 
-        if not isJump:
-            if keys[pygame.K_SPACE]:
-                isJump = True
-        else:
-            jump()
+
+    if punch_button == 1:
+        punch()
+
+    if jump_button == 1:
+        jump()
+    
     pygame.draw.rect(win,(0,255,0),(bag_x,bag_y,width,height))
     pygame.draw.rect(win,(255,0,0),(player_x,player_y,width,height))
     pygame.display.update()
-
-camera.release()
-cv2.distoryAllWindows()
