bc982429eef970731f0eb84ff719285b4e4df9f1
daythekid <37392632+daythekid@users.noreply.github.com>
2019-01-20 13:41:10 -0500

    added collision and tests

--- Development/Game.py
+++ Development/Game.py
@@ -1,5 +1,13 @@
+"""
+Tests
+Pep8
+Lists
+Functions w/ perameters
+"""
+
 import pygame
 import cv2
+import sys
 import threading
 import random
 
@@ -22,36 +30,38 @@ except:
     print("There was an error finding your joystick.")
     print("Please use Z and X instead")
 
+
 pygame.mixer.pre_init(44100, 16, 2, 4096)
 pygame.mixer.music.load("music.mp3")
 pygame.mixer.music.set_volume(0.5)
 pygame.mixer.music.play(-1)
 
-jump_button = 0
-punch_button = 0
 
 pygame.font.init()
 myfont = pygame.font.SysFont('Comic Sans MS', 30)
 
 run = True
 isMenu = True
-'''
+
 def opencv():
     global position
-    camera = cv2.VideoCapture(0)
+    try:
+        camera = cv2.VideoCapture(0)
+    except:
+        print("There was no webcam detected")
+        print("Please try again")
     face_cascade = cv2.CascadeClassifier("face.xml")
     while run:
         ret, img = camera.read()
         # Converts video feed to gray
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
         for (x, y, w, h) in faces:
             position = x + (w/2)
 
 opencv_thread = threading.Thread(name="Facial Detection", target=opencv)
 opencv_thread.start()
-'''
+
 width = 125
 height = 300
 position = 0
@@ -68,6 +78,9 @@ player_health = 100
 startPunch = False
 punchTimer = 10
 punchCooldown = 0
+
+jump_button = 0
+punch_button = 0
 
 # Variables for bot
 bot_x = displayWidth - width - 100
@@ -92,64 +105,15 @@ begin_prompt_rect = begin_prompt.get_rect()
 background = pygame.transform.scale(pygame.image.load("background.gif"), (1920, 1080))
 background_rect = background.get_rect()
 
+#Winning and losing screens
 win_screen = pygame.transform.scale(pygame.image.load("win_screen.jpg"), (1920, 1080))
 win_rect = win_screen.get_rect()
 lose_screen = pygame.transform.scale(pygame.image.load("lose_screen.jpg"), (1920, 1080))
 lose_rect = lose_screen.get_rect()
 
-while run:
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_x:
-                jump_button = 1
-            if event.key == pygame.K_z:
-                punch_button = 1
-
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_x:
-                jump_button = 0
-            if event.key == pygame.K_z:
-                punch_button = 0
-    
-    if useJoystick:
-        jump_button = joystick.get_button(0)
-        punch_button = joystick.get_button(1)
-
-    win.blit(background, background_rect)
-
-    if isMenu:
-        win.blit(title, (displayWidth/2 - title_rect.width/2, displayHeight/6))
-        win.blit(begin_prompt, (displayWidth/2 - begin_prompt_rect.width/2, 6*displayHeight/7))
-        pygame.display.update()
-
-        if jump_button == 1 or punch_button == 1:
-            isMenu = False
-        continue
-
-    if (450 - position) * 5 + width < bot_x:
-        player_x = (450 - position) * 5
-
-    # Player Control
-    if punchCooldown == 0:
-        if punch_button == 1:
-            startPunch = True
-    else:
-        punchCooldown -= 1
-    
-    if startPunch:
-        punchTimer -= 1
-        pygame.draw.rect(win, (255, 0, 0), (player_x + width, player_y + 100, 100, 50))
-        if punchTimer == 0:
-            if player_x + width + 100 > bot_x and not botJump:
-                bot_health -= 5
-            startPunch = False
-            punchTimer = 10
-            punchCooldown = 30
-
+def jump():
+    global player_y, playerJump, jumpCount, jumpCooldown
+    global jump_button
     if jumpCooldown == 0:
         if jump_button == 1:
             playerJump = True
@@ -168,6 +132,83 @@ while run:
             jumpCount = 15
             jumpCooldown = 50
 
+def out_of_screen(player_x):
+    if player_x < 0:
+        return True
+    return False
+
+def collision(player_x,width,bot_x):
+    if player_x + width >= bot_x:
+        return True
+    return False
+
+def test():
+    assert out_of_screen(-132) == True
+    assert out_of_screen(32) == False
+    assert collision(230,60,260) == True
+    assert collision(132,30,152) == True
+    assert collision(120,32,580) == False
+
+
+
+test()
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_x:
+                    jump_button = 1
+                if event.key == pygame.K_z:
+                    punch_button = 1
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_x:
+                jump_button = 0
+            if event.key == pygame.K_z:
+                punch_button = 0
+
+    if useJoystick:
+        jump_button = joystick.get_button(0)
+        punch_button = joystick.get_button(1)
+    
+    win.blit(background, background_rect)
+
+    if isMenu:
+        win.blit(title, (displayWidth/2 - title_rect.width/2, displayHeight/6))
+        win.blit(begin_prompt, (displayWidth/2 - begin_prompt_rect.width/2, 6*displayHeight/7))
+        pygame.display.update()
+
+        if jump_button == 1 or punch_button == 1:
+            isMenu = False
+        continue
+
+    if (450 - position) * 5 + width < bot_x:
+        player_x = (450 - position) * 5
+
+    if out_of_screen(player_x):
+        player_x = 0
+    
+    # Player Control
+    jump()
+    if punchCooldown == 0:
+        if punch_button == 1:
+            startPunch = True
+    else:
+        punchCooldown -= 1
+    
+    if startPunch:
+        punchTimer -= 1
+        pygame.draw.rect(win, (255, 0, 0), (player_x + width, player_y + 100, 100, 50))
+        if punchTimer == 0:
+            if player_x + width + 100 > bot_x:
+                bot_health -= 5
+            startPunch = False
+            punchTimer = 10
+            punchCooldown = 30
+
+    
     # Bot movement and fighting
     if bot_jumpCooldown == 0:
         if punchTimer == 1:
